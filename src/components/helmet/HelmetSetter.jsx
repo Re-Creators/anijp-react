12c91c6ad6b14ddf5d6f42a222f5662ca5084566
= <richardoehbet221@gmail.com>
2022-02-02 13:05:52 +0700

    remove unused code

--- src/components/helmet/HelmetSetter.jsx
+++ src/components/helmet/HelmetSetter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { Helmet } from "react-helmet";
 import { useSelector } from "react-redux";
 import { selectHelmetTitle } from "../../features/helmet-title/helmetTitleSlice";
@@ -8,7 +8,6 @@ import {
 } from "../../features/music-player/musicPlayerSlice";
 
 function HelmetSetter() {
-  console.log("HELMET RENDER");
   const title = useSelector(selectHelmetTitle);
   const isPlaying = useSelector(selectIsPlaying);
   const activeSong = useSelector(selectActiveSong);
