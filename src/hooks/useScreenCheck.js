77b3fb0e2295801b2cd0b965f6636fbd11710972
= <richardoehbet221@gmail.com>
2022-04-02 09:45:05 +0700

    refactor : change logic for checking mobile screen

--- src/hooks/useScreenCheck.js
+++ src/hooks/useScreenCheck.js
@@ -6,26 +6,12 @@ export default function useScreenCheck() {
   useEffect(() => {
     let hasTouchScreen = false;
 
-    if (window.innerWidth < 640) {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
       hasTouchScreen = true;
-    } else if ("maxTouchPoints" in navigator) {
-      hasTouchScreen = navigator.maxTouchPoints > 0 && window.innerWidth < 640;
-    } else if ("msMaxTouchPoints" in navigator) {
-      hasTouchScreen =
-        navigator.msMaxTouchPoints > 0 && window.innerWidth < 640;
-    } else {
-      var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
-      if (mQ && mQ.media === "(pointer:coarse)") {
-        hasTouchScreen = !!mQ.matches;
-      } else if ("orientation" in window) {
-        hasTouchScreen = true; // deprecated, but good fallback
-      } else {
-        // Only as a last resort, fall back to user agent sniffing
-        var UA = navigator.userAgent;
-        hasTouchScreen =
-          /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-          /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
-      }
     }
     setScreen(hasTouchScreen ? "MOBILE" : "DESKTOP");
   }, []);
